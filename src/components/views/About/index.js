caf87a54d527935f65785f0e0377ede558bf31b2
Matthew Lo <mamlo@ucsc.edu>
2018-09-01 22:29:17 -0700

    Added some text in about page

--- src/components/views/About/index.js
+++ src/components/views/About/index.js
@@ -5,7 +5,15 @@ class About extends Component {
 	render () {
 		return(
 			<div className="About">
-				<div className="bg"></div>
+				<div className="bg">
+					<div className="aboutText">
+						Hey! I&#39;m Matthew, a programmer, board game enthusiast, and a huge fan of sitcoms, especially The Office. 
+						I&#39;ve always been a problem solver and love challenges and riddles! My favorite thing about programming is the 
+						endless stream of complex problems that are being solved. Technology is always evolving and I hope to continue learning 
+						new things about software development. I built this website as a way to challenge myself to learn technologies that I 
+						may not be familiar with. Please take a look around!
+					</div>
+				</div>
 			</div>
 		);
 	}
--- src/components/views/Navigation/index.js
+++ src/components/views/Navigation/index.js
@@ -35,6 +35,7 @@ class Navigation extends Component {
       }));
     }
   }
+
 	render() {
 		return (
 			<div className="Navigation">
